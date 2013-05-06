c755dbf418dad5696a5cf8894072cbcf741d0ed6
ikarosdev@gmail.com <ikarosdev@gmail.com>
2013-05-05 19:33:24 -0500

    Add wifi defines. We have a Broadcom chip, so we need to reflect that.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,17 @@ TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 BOARD_EGL_CFG := device/lge/ms870/prebuilt/lib/egl/egl.cfg
 
-# Wifi
+# Wifi related defines
+WIFI_DRIVER_MODULE_NAME          := wlan
+WIFI_BAND                        := 802_11_ABG
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -98,18 +108,6 @@ TARGET_FORCE_CPU_UPLOAD := true
 
 # Preload bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
-
-# Wifi related defines
-
-# Filesystem related
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1744829440
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2550136320
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Recovery
 
 # Inherit from the proprietary version
 -include vendor/lge/ms870/BoardConfigVendor.mk
